f8eed4ba07652dfc8842d6e4a22c1b56aa9a7255
etiennebeaulieu <61425467+etiennebeaulieu@users.noreply.github.com>
2021-02-19 13:53:02 -0500

    Update Sport.java

--- app/src/main/java/modele/Sport.java
+++ app/src/main/java/modele/Sport.java
@@ -1,16 +1,12 @@
 package modele;
 
-import android.media.Image;
-import android.net.Uri;
 
 public enum Sport {
     RANDONNEE("Randonnée pédestre"), COURSE("Course à pied"), VELO("Vélo"), RAQUETTE("Raquette"), SKI_RANDONNEE("Ski de randonnée"), SKI("Ski alpin"), PATIN("Patin à glace"), SKI_FOND("Ski de fond");
 
 private final String nom;
-private final Image icon;
 
     Sport(String nom) {
         this.nom = nom;
-        this.icon = new Image(R.dr);
     }
 }

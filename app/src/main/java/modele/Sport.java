b24a79edd65a43830f6598755a8a9ceb94a31fb5
etiennebeaulieu <61425467+etiennebeaulieu@users.noreply.github.com>
2021-02-23 19:54:32 -0500

    Enum Sport fini

--- app/src/main/java/modele/Sport.java
+++ app/src/main/java/modele/Sport.java
@@ -1,12 +1,24 @@
 package modele;
 
 
+import android.net.Uri;
+
 public enum Sport {
-    RANDONNEE("Randonnée pédestre"), COURSE("Course à pied"), VELO("Vélo"), RAQUETTE("Raquette"), SKI_RANDONNEE("Ski de randonnée"), SKI("Ski alpin"), PATIN("Patin à glace"), SKI_FOND("Ski de fond");
+    RANDONNEE("Randonnée pédestre", "android.resource://main/drawable/rando.png"), COURSE("Course à pied", "android.resource://main/drawable/course"), VELO("Vélo", "android.resource://main/drawable/velo"), RAQUETTE("Raquette", "android.resource://main/drawable/raquette"), SKI_RANDONNEE("Ski de randonnée", "android.resource://main/drawable/ski_rando"), SKI("Ski alpin", "android.resource://main/drawable/ski_alpin"), PATIN("Patin à glace", "android.resource://main/drawable/patin"), SKI_FOND("Ski de fond", "android.resource://main/drawable/ski_fond");
 
-private final String nom;
+    private final String nom;
+    private final Uri uriImage;
 
-    Sport(String nom) {
+    Sport(String nom, String uri) {
         this.nom = nom;
+        this.uriImage = Uri.parse(uri);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public Uri getUriImage() {
+        return uriImage;
     }
 }

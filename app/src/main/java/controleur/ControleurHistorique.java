47573363df5b8b0377d78eb51727666a6d347759
etiennebeaulieu <61425467+etiennebeaulieu@users.noreply.github.com>
2021-02-23 20:14:07 -0500

    Debut controleur historique

--- app/src/main/java/controleur/ControleurHistorique.java
+++ app/src/main/java/controleur/ControleurHistorique.java
@@ -1,4 +1,19 @@
 package controleur;
 
+import java.util.List;
+
+import modele.Activite;
+
 public class ControleurHistorique {
+
+    private List<Activite> listeActivite;
+
+
+    public List<Activite> getListeActivite() {
+        return listeActivite;
+    }
+
+    public void setListeActivite(List<Activite> listeActivite) {
+        this.listeActivite = listeActivite;
+    }
 }

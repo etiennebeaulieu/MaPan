21631ed8ccdbc4c0b6c506bc982e7f6197febc6a
zacharieforest <61426839+zacharieforest@users.noreply.github.com>
2021-02-26 12:21:12 -0500

    Update ControleurParametre.java

--- app/src/main/java/controleur/ControleurParametre.java
+++ app/src/main/java/controleur/ControleurParametre.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +13,9 @@ import com.example.mapan.R;
 
 public class ControleurParametre extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    String[] noms = { "Sport - Date", "Date", "Date - Durée", "Date - Distance"};
-    String[] sport = { "Course à pied", "Randonnée pédestre", "Vélo", "Raquette", "Ski de randonnée", "Ski alpin", "Patin à glace", "Ski de fond"};
-
+    String[] nom = { "Sport - Date", "Date", "Date - Durée", "Date - Distance"};
+    String[] type = { "Course à pied", "Randonnée pédestre", "Vélo", "Raquette", "Ski de randonnée", "Ski alpin", "Patin à glace", "Ski de fond"};
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +24,34 @@ public class ControleurParametre extends AppCompatActivity implements AdapterVie
         setContentView(R.layout.parametre);
 
         SharedPreferences pref = getApplicationContext().getSharedPreferences("Preferences", 0);
-        SharedPreferences.Editor editor = pref.edit();
+        editor = pref.edit();
 
         Spinner spin_nom = (Spinner) findViewById(R.id.choix_nom_defaut);
         spin_nom.setOnItemSelectedListener(this);
 
-        Spinner spin_sport = (Spinner) findViewById(R.id.choix_type_defaut);
-        spin_sport.setOnItemSelectedListener(this);
+        ArrayAdapter aa1 = new ArrayAdapter(this,android.R.layout.simple_spinner_item,nom);
+        aa1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin_nom.setAdapter(aa1);
+
+
+        Spinner spin_type = (Spinner) findViewById(R.id.choix_type_defaut);
+        spin_type.setOnItemSelectedListener(this);
+
+        ArrayAdapter aa2 = new ArrayAdapter(this,android.R.layout.simple_spinner_item,type);
+        aa2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin_type.setAdapter(aa2);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+    switch(parent.getId()){
+        case R.id.choix_nom_defaut:{
+            editor.putString("nom_défaut", parent.getItemAtPosition(position).toString());
+        }
+        case R.id.choix_type_defaut:{
+            editor.putString("type_défaut", parent.getItemAtPosition(position).toString());
+        }
+    }
     }
 
     @Override

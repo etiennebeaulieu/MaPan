24fb3cbe9e7284d71c74bb3152509732f3c8c6f5
etiennebeaulieu <61425467+etiennebeaulieu@users.noreply.github.com>
2021-02-26 09:48:49 -0500

    Update Fichiers.java

--- app/src/main/java/modele/Fichiers.java
+++ app/src/main/java/modele/Fichiers.java
@@ -1,10 +1,45 @@
 package modele;
+
 import org.jdom2.*;
+
+import java.util.ArrayList;
+
+import modele.Activite;
+
 public class Fichiers {
 
-    public void lireFichier()
-    {
-        
+    public void lireFichier() {
+
+    }
+
+    public void ecrireFichier(Activite activite) {
+        ArrayList<Double> tabLatitude = activite.getTabLatitude();
+        ArrayList<Double> tabLongitude = activite.getTabLongitude();
+        ArrayList<Double> tabElevation = activite.getTabElevationMetrique();
+        ArrayList<Date> tabTemps = activite.getTabTemps();
+
+        try {
+            Element gpx = new Element("gpx");
+            Document doc = new Document(gpx);
+            doc.setRootElement(gpx);
+
+
+            Element trace = new Element("trk");
+            trace.addContent(new Element("name").setText(activite.getNom()));
+
+            Element segment = new Element("trkseg");
+
+            for (int i = 0; i < tabLatitude.size(); i++) {
+                Element point = new Element("trkpt")
+                point.setAttribute(new Attribute("lat", tabLatitude.get(i).toString()));
+                point.setAttribute(new Attribute("lon", tabLongitude.get(i).toString()));
+                point.addContent(new Element("ele").setText(tabElevation.get(i).toString()));
+                point.addContent(new Element("time").setText(tabTemps.get(i).toString));
+
+                segment.addContent(point);
+            }
+
+        }
     }
 
 }

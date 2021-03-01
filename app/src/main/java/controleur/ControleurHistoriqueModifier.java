f2facbd3d3ff80c315273fa617f5b61a3c601886
etiennebeaulieu <61425467+etiennebeaulieu@users.noreply.github.com>
2021-03-01 15:29:41 -0500

    Selection item

--- app/src/main/java/controleur/ControleurHistoriqueModifier.java
+++ app/src/main/java/controleur/ControleurHistoriqueModifier.java
@@ -1,17 +1,22 @@
 package controleur;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -32,14 +37,19 @@ import com.example.mapan.R;
 import modele.Activite;
 import modele.ActiviteAdapter;
 
+import static com.example.mapan.R.id.modifier_exporter;
+
 public class ControleurHistoriqueModifier extends AppCompatActivity{
 
     private ListView modifier_list;
+    private ImageButton modifier_exporter;
     private ArrayList<Activite> listeActivites;
     private ControleurHistorique historique;
     private boolean isDistanceMetrique = true;
     private ActiviteAdapter adapter;
+    private Activite activiteSelect;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,8 +57,21 @@ public class ControleurHistoriqueModifier extends AppCompatActivity{
 
         listeActivites = new ArrayList<Activite>();
         modifier_list = findViewById(R.id.modifier_list);
+        modifier_exporter = findViewById(R.id.modifier_exporter);
+        activiteSelect = null;
+
         adapter = new ActiviteAdapter(this, R.layout.list_row, listeActivites);
         modifier_list.setAdapter(adapter);
+
+        modifier_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                activiteSelect = (Activite) modifier_list.getItemAtPosition(position);
+            }
+        });
+
+
+
 
         loadActivites();
         adapter.notifyDataSetChanged();
@@ -65,7 +88,10 @@ public class ControleurHistoriqueModifier extends AppCompatActivity{
     }
 
     public void exporterGPX(View view){
-        modifier_list.getSelectedItem();
+
+        Toast.makeText(ControleurHistoriqueModifier.this, activiteSelect.getNom(), Toast.LENGTH_SHORT).show();
+        partager(activiteSelect);
+
     }
 
     public void loadActivites(){

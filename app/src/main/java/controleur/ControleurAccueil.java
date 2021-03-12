3ce6e6aaa7430552d7b3942bf18a01341f4ca739
etiennebeaulieu <61425467+etiennebeaulieu@users.noreply.github.com>
2021-03-12 09:21:13 -0500

    Deébut sprint 2

--- app/src/main/java/controleur/ControleurAccueil.java
+++ app/src/main/java/controleur/ControleurAccueil.java
@@ -1,4 +1,19 @@
 package controleur;
 
-public class ControleurAccueil {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mapan.R;
+
+public class ControleurAccueil extends AppCompatActivity {
+
+    private MapboxMap mapboxMap;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.accueil);
+    }
 }

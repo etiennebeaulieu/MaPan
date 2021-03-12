128343f5d48e69f41cb0778407ff7fe4e5e74e49
zacharieforest <79326893+ZacharieForest17@users.noreply.github.com>
2021-03-12 12:02:57 -0500

    Update Sport.java

--- app/src/main/java/modele/Sport.java
+++ app/src/main/java/modele/Sport.java
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import com.example.mapan.R;
 
 public enum Sport {
-    RANDONNEE("Randonnée pédestre", R.drawable.rando), COURSE("Course à pied", R.drawable.course), VELO("Vélo", R.drawable.velo), RAQUETTE("Raquette", R.drawable.raquette), SKI_RANDONNEE("Ski de randonnée", R.drawable.ski_rando), SKI("Ski alpin", R.drawable.ski_alpin), PATIN("Patin à glace", R.drawable.patin), SKI_FOND("Ski de fond", R.drawable.ski_fond);
+    RANDONNEE("Randonnée pédestre", R.drawable.rando), COURSE("Course à pied", R.drawable.course), VELO("Vélo", R.drawable.velo), RAQUETTE("Raquette", R.drawable.raquette), SKI_RANDONNEE("Ski de randonnée", R.drawable.ski_rando), SKI("Ski alpin", R.drawable.ski_alpin), PATIN("Patin à glace", R.drawable.patin), SKI_FOND("Ski de fond", R.drawable.ski_fond), AUTRE("Autre",R.drawable.autre);
 
     private final String nom;
     private final int image;

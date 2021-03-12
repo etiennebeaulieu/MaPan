3df28a5192010b0686bca8f543ef93806e411b50
Xouby <dussaultxavier@hotmail.com>
2021-03-12 11:19:38 -0500

    modification nouvelle activite + xml

--- app/src/main/java/controleur/ControleurNouvelleActivite.java
+++ app/src/main/java/controleur/ControleurNouvelleActivite.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +21,10 @@ public class ControleurNouvelleActivite extends AppCompatActivity
 {
     private EditText nouveau_nom;
     private ListView nouvelle_activitesList;
+    private ImageButton nouvelle_annuler;
+    private ImageButton commencer;
+    private ImageButton parametre;
+
 
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -28,6 +33,10 @@ public class ControleurNouvelleActivite extends AppCompatActivity
 
         nouveau_nom = (EditText)findViewById(R.id.nouveau_nom);
         nouvelle_activitesList = (ListView)findViewById(R.id.nouvelle_activitesList);
+        nouvelle_annuler = (ImageButton)findViewById(R.id.nouvelle_annuler);
+        commencer = (ImageButton)findViewById(R.id.commencer);
+        parametre = (ImageButton)findViewById(R.id.parametre);
+
 
     }
 
@@ -36,14 +45,17 @@ public class ControleurNouvelleActivite extends AppCompatActivity
             String nom = nouveau_nom.getText().toString();
             Sport sp = (Sport) nouvelle_activitesList.getSelectedItem();
 
-if(nom != null && sp!= null)
-{
-
-    Activite act = new Activite(nom, sp, new File());
-}
+        if(nom != null && sp!= null)
+        {
+          Activite act = new Activite(nom, sp);
+        }
         }
 
     public void ouvrirParametre(View view) {
         startActivity(new Intent(ControleurNouvelleActivite.this, ControleurParametre.class));
+    }
+
+    public void retourAcceuil(View view) {
+        startActivity(new Intent(ControleurNouvelleActivite.this, ControleurAcceuil.class));
     }
 }

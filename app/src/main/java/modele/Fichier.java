4f89e2719b669deede95abc06a98495e6ad6f228
etiennebeaulieu <61425467+etiennebeaulieu@users.noreply.github.com>
2021-03-25 13:22:38 -0400

    Update Fichier.java

--- app/src/main/java/modele/Fichier.java
+++ app/src/main/java/modele/Fichier.java
@@ -80,6 +80,9 @@ public class Fichier {
         File dossierActivite = new File(directory, "activites");
         if(!dossierActivite.exists())
             dossierActivite.mkdir();
+
+        new File(dossierActivite, nom + ".mp").delete();
+
     }
 
     public static File partager(Context context, Activite activite){

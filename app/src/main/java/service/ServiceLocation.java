7ac23d00eea2c23d4942d3c12ac58f94ee3567a4
etiennebeaulieu <61425467+etiennebeaulieu@users.noreply.github.com>
2021-04-06 19:22:05 -0400

    Service

--- app/src/main/java/service/ServiceLocation.java
+++ app/src/main/java/service/ServiceLocation.java
@@ -1,5 +1,24 @@
 package service;
 
-public class ServiceLocation {
+import android.app.Activity;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
 
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+
+public class ServiceLocation extends Service {
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }

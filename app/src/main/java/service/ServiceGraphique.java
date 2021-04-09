9c6353f23b44e1d2ee9136385ce08ba4d4135b4b
yannichambo <yannichambo@gmail.com>
2021-04-09 12:27:42 -0400

    Update ServiceGraphique.java

--- app/src/main/java/service/ServiceGraphique.java
+++ app/src/main/java/service/ServiceGraphique.java
@@ -1,5 +1,32 @@
 package service;
 
-public class ServiceGraphique
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+public class ServiceGraphique extends Service
 {
+    private LineChart graphique;
+    private LineData data;
+    private LineDataSet set;
+
+    @Override
+    public void onCreate(){
+        
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) { return null; }
 }
